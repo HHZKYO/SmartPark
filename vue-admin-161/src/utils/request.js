dcfc43f1272455aa3fcad7c54b5ce00aaa84440e
HHZKYO <1692189552@qq.com>
2024-12-11 21:38:49 +0800

    接口错误统一处理&token失效处理

--- vue-admin-161/src/utils/request.js
+++ vue-admin-161/src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import store from '@/store'
+import { Message } from 'element-ui'
+import router from '@/router'
 const service = axios.create({
   baseURL: 'https://api-hmzs.itheima.net/v1',
   timeout: 5000 // request timeout
@@ -22,6 +24,13 @@ service.interceptors.response.use(
     return response.data
   },
   error => {
+    Message.error(error.response.data.msg)
+    if (error.response.status === 401) {
+      // 1. 跳转到登录
+      router.push('/login')
+      // 2. 清空用户数据
+      store.commit('user/delTokens')
+    }
     return Promise.reject(error)
   }
 )
