16112cdf2de7002030347fa6bf0d7a05c16a66f0
HHZKYO <1692189552@qq.com>
2024-12-25 09:48:13 +0800

    月卡管理-新增月卡车辆信息表单校验

--- vue-admin-161/src/views/Car/CarCard/addCard.vue
+++ vue-admin-161/src/views/Car/CarCard/addCard.vue
@@ -7,18 +7,18 @@
       <div class="form-container">
         <div class="title">车辆信息</div>
         <div class="form">
-          <el-form label-width="100px">
-            <el-form-item label="车主姓名">
-              <el-input />
+          <el-form :model="carInfoForm" :rules="carInfoRules" label-width="100px">
+            <el-form-item label="车主姓名" prop="personName">
+              <el-input v-model="carInfoForm.personName" />
             </el-form-item>
-            <el-form-item label="联系方式">
-              <el-input />
+            <el-form-item label="联系方式" prop="phoneNumber">
+              <el-input v-model="carInfoForm.phoneNumber" />
             </el-form-item>
-            <el-form-item label="车辆号码">
-              <el-input />
+            <el-form-item label="车辆号码" prop="carNumber">
+              <el-input v-model="carInfoForm.carNumber" />
             </el-form-item>
-            <el-form-item label="车辆品牌">
-              <el-input />
+            <el-form-item label="车辆品牌" prop="carBrand">
+              <el-input v-model="carInfoForm.carBrand" />
             </el-form-item>
           </el-form>
         </div>
@@ -65,7 +65,49 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      // 车辆信息表单对象
+      carInfoForm: {
+        personName: '',
+        phoneNumber: '',
+        carNumber: '',
+        carBrand: ''
+      },
+      // 车辆信息校验对象
+      carInfoRules: {
+        personName: [
+          {
+            required: true, message: '请输入车主姓名', trigger: 'blur'
+          },
+          {
+            pattern: /^[\u4e00-\u9fa5]{2,10}$/, message: '只能是中文', trigger: 'blur'
+          }
+        ],
+        phoneNumber: [
+          {
+            required: true, message: '请输入联系方式', trigger: 'blur'
+          },
+          { pattern: /^1[34578]\d{9}$/, message: '请输入正确格式手机号' }
+        ],
+        carNumber: [
+          {
+            required: true, message: '请输入车辆号码', trigger: 'blur'
+          },
+          {
+            pattern:  /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使]{1}[A-Z]{1}[A-Z0-9]{5,6}$/,
+            message: '请输入正确的车牌号',
+            trigger: 'blur'
+          }
+        ],
+        carBrand: [
+          {
+            required: true, message: '请输入车辆品牌', trigger: 'blur'
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
 
