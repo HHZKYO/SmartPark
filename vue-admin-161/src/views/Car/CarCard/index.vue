851fe2c762de0b9202181780ad63ef9a22b70b5d
HHZKYO <1692189552@qq.com>
2024-12-26 09:35:56 +0800

    删除时跳出确认框

--- vue-admin-161/src/views/Car/CarCard/index.vue
+++ vue-admin-161/src/views/Car/CarCard/index.vue
@@ -139,9 +139,24 @@ export default {
       this.$router.push(`/add-card?id=${id}`)
     },
     // 删除
-    async del(id) {
-      await delCardAPI(id)
-      this.getList()
+    del(id) {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        delCardAPI(id)
+        this.getList()
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
     }
   }
 }
