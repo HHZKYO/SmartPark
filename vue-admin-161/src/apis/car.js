f141c575e72017360be97cba75aa649d30b13929
HHZKYO <1692189552@qq.com>
2025-01-18 12:43:47 +0800

    行车管理-计费规则管理-准备模板

--- vue-admin-161/src/apis/car.js
+++ vue-admin-161/src/apis/car.js
@@ -61,3 +61,15 @@ export function delCardAPI(id) {
     method: 'DELETE'
   })
 }
+
+/**
+ * 获取规则列表
+ * @param { page, pageSize} params
+ * @returns
+ */
+export function getRuleListAPI(params) {
+  return request({
+    url: 'parking/rule/list',
+    params
+  })
+}
