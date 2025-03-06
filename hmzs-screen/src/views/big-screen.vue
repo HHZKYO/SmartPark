e9b82d2287136637b43b0cb253be4c94e8d8ef37
HHZKYO <1692189552@qq.com>
2025-03-06 09:00:40 +0800

    大屏可视化- calescreen 的使用

--- hmzs-screen/src/views/big-screen.vue
+++ hmzs-screen/src/views/big-screen.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <h1>大屏</h1>
+    <!-- 3.告诉适配组件我是按照多少宽高设计图实现的 -->
+    <VScaleScreen width="1920" height="1080">
+      <!-- <img src="@/assets/img.png" alt=""> -->
+      <div class="all-charts" ></div>
+      <div class="model-container">
+        <div class="canvas-3d"></div>
+      </div>
+    </VScaleScreen>
   </div>
 </template>
 
 <script setup>
-   
+  // 目标：缩放适配
+  // 1.下载v-scale-screen
+  // 2.引入插件暴露的组件使用
+  import VScaleScreen from 'v-scale-screen'
 </script>
 
 <style lang='scss' scoped>
