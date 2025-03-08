e533f5ad4afd420f58da6290b91291bc86632c69
HHZKYO <1692189552@qq.com>
2025-03-08 12:11:46 +0800

    大屏可视化-2D-园区年度收入分析数据展示

--- hmzs-screen/src/views/big-screen.vue
+++ hmzs-screen/src/views/big-screen.vue
@@ -13,8 +13,13 @@
           <div class="icons-container">
             <div class="item">
               <div class="icons-item building-icon">
-                <span class="number">
-                  {{ parkInfo.buildingTotal }}
+                <span class="number">               
+                  <!-- 首次渲染（后端数据没有返回）
+                  parkInfo - {} - {}.base -> undefined -> undefined.buidingTotal
+                  二次渲染（后端数据返回之后 响应式状态发生变化 再次引起模版渲染）
+                  场景：访问一个空对象多层次的属性
+                  解决办法：可选链?.   保证必须前面有值才会继续往后进行点运算 -->
+                  {{ parkInfo.base?.buildingTotal }}
                 </span>
               </div>
               <span class="title">楼宇总数</span>
@@ -23,7 +28,7 @@
             <div class="item">
               <div class="icons-item enterprise-icon">
                 <span class="number">
-                  {{ parkInfo.enterpriseTotal }}
+                  {{ parkInfo.base?.enterpriseTotal }}
                 </span>
               </div>
               <span class="title">入驻企业总数</span>
@@ -32,7 +37,7 @@
             <div class="item">
               <div class="icons-item car-icon">
                 <span class="number">
-                  {{ parkInfo.parkingTotal }}
+                  {{ parkInfo.base?.parkingTotal }}
                 </span>
               </div>
               <span class="title">车位总数</span>
@@ -41,13 +46,27 @@
             <div class="item">
               <div class="icons-item rod-icon">
                 <span class="number">
-                  {{ parkInfo.chargePoleTotal }}
+                  {{ parkInfo.base?.chargePoleTotal }}
                 </span>
               </div>
               <span class="title">一体杆总数</span>
               <span class="unity">（个）</span>
             </div>
           </div>
+        </div>
+        <div class="section-two">
+          <img class="img-header"
+            src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E5%B9%B4%E5%BA%A6%E6%94%B6%E5%85%A5%E5%88%86%E6%9E%90%402x.png"
+            alt="" />
+          <div class="bar-chart-titile">
+            <span>单位：元</span>
+            <div>
+              <span class="bar-icon blue-bar-icon"></span>
+              <span class="bar-icon red-bar-icon"></span>
+              收入情况
+            </div>
+          </div>
+          <div class="bar-chart" ref="barChart"></div>
         </div>
       </div>
       <!-- 3D部分 -->
@@ -65,13 +84,87 @@
   import VScaleScreen from 'v-scale-screen'
   import { ref } from 'vue'
   import { getParkInfoAPI } from '@/apis/index'
+  import * as echarts from 'echarts'
+
+  // 园区概况
   const parkInfo = ref({})
   const getParkInfo = async () => {
     const res = await getParkInfoAPI()
     console.log(res)
-    parkInfo.value = res.data.base
+    parkInfo.value = res.data
+    initBarChart() // 既保证有数据，也保证了 DOM 加载完毕
   }
   getParkInfo()
+
+  // 园区收入分析
+  // 渲染年度收入分析图表
+  const barChart = ref(null) // 关联具体的容器盒子标签
+  const initBarChart = () => { // 创建图表函数
+    // 1. 解构图表数据
+    const { parkIncome } = parkInfo.value
+    // 2. == 根据业务：准备好的options数据 ==
+    const barOptions = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow',
+        },
+      },
+      grid: {
+        // 让图表占满容器
+        top: '10px',
+        left: '0px',
+        right: '0px',
+        bottom: '0px',
+        containLabel: true,
+      },
+      xAxis: [
+        {
+          type: 'category',
+          axisTick: {
+            alignWithLabel: true,
+            show: false,
+          },
+          // x轴绑定数据
+          data: parkIncome.xMonth,
+        },
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          splitLine: {
+            show: false,
+          },
+        },
+      ],
+      series: [
+        {
+          name: '园区年度收入',
+          type: 'bar',
+          barWidth: '10px',
+          data: parkIncome.yIncome.map((item, index) => {
+            const color =
+              index % 2 === 0
+                ? new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  { offset: 0.23, color: '#74c0f8' },
+                  { offset: 1, color: 'rgba(116,192,248,0.00)' },
+                ])
+                : new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  { offset: 0.23, color: '#ff7152' },
+                  { offset: 1, color: 'rgba(255,113,82,0.00)' },
+                ])
+            return { value: item, itemStyle: { color } }
+          }),
+        },
+      ],
+      textStyle: {
+        color: '#B4C0CC',
+      },
+    }
+    // 3. 渲染图表
+    const myBarChart = echarts.init(barChart.value)
+    myBarChart.setOption(barOptions)
+  }
 </script>
 
 <style lang='scss' scoped>
@@ -145,6 +238,17 @@
         margin-top: 8px;
       }
     }
+  }
+}
+
+.section-two {
+  // 主轴宽度（y轴）
+  flex-basis: 35%;
+  margin-top: 50px;
+
+  .bar-chart {
+    width: 100%;
+    height: calc(100% - 90px);
   }
 }
 </style>
