9ebf9d085b840eb432235c064f39719ba55535fc
HHZKYO <1692189552@qq.com>
2025-03-09 07:25:30 +0800

    大屏可视化-2D-园区产业分布-饼状图展示

--- hmzs-screen/src/views/big-screen.vue
+++ hmzs-screen/src/views/big-screen.vue
@@ -54,6 +54,7 @@
             </div>
           </div>
         </div>
+        <!-- 园区年度收入分析 -->
         <div class="section-two">
           <img class="img-header"
             src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E5%B9%B4%E5%BA%A6%E6%94%B6%E5%85%A5%E5%88%86%E6%9E%90%402x.png"
@@ -68,6 +69,13 @@
           </div>
           <div class="bar-chart" ref="barChart"></div>
         </div>
+        <!-- 园区产业分布 -->
+        <div class="section-three">
+          <img class="img-header"
+            src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E4%BA%A7%E4%B8%9A%E5%88%86%E5%B8%83%402x.png"
+            alt="" />
+          <div class="pie-chart" ref="pieChart"></div>
+        </div>
       </div>
       <!-- 3D部分 -->
       <div class="model-container">
@@ -78,93 +86,143 @@
 </template>
 
 <script setup>
-  // 目标：缩放适配
-  // 1.下载v-scale-screen
-  // 2.引入插件暴露的组件使用
-  import VScaleScreen from 'v-scale-screen'
-  import { ref } from 'vue'
-  import { getParkInfoAPI } from '@/apis/index'
-  import * as echarts from 'echarts'
+// 目标：缩放适配
+// 1.下载v-scale-screen
+// 2.引入插件暴露的组件使用
+import VScaleScreen from 'v-scale-screen'
+import { ref } from 'vue'
+import { getParkInfoAPI } from '@/apis/index'
+import * as echarts from 'echarts'
 
-  // 园区概况
-  const parkInfo = ref({})
-  const getParkInfo = async () => {
-    const res = await getParkInfoAPI()
-    console.log(res)
-    parkInfo.value = res.data
-    initBarChart() // 既保证有数据，也保证了 DOM 加载完毕
+// 园区概况
+const parkInfo = ref({})
+const getParkInfo = async () => {
+  const res = await getParkInfoAPI()
+  console.log(res)
+  parkInfo.value = res.data
+  initBarChart() // 既保证有数据，也保证了 DOM 加载完毕
+  initPieChart()
+}
+getParkInfo()
+
+// 园区收入分析
+// 渲染年度收入分析图表
+const barChart = ref(null) // 关联具体的容器盒子标签
+const initBarChart = () => { // 创建图表函数
+  // 1. 解构图表数据
+  const { parkIncome } = parkInfo.value
+  // 2. == 根据业务：准备好的options数据 ==
+  const barOptions = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
+    },
+    grid: {
+      // 让图表占满容器
+      top: '10px',
+      left: '0px',
+      right: '0px',
+      bottom: '0px',
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: 'category',
+        axisTick: {
+          alignWithLabel: true,
+          show: false,
+        },
+        // x轴绑定数据
+        data: parkIncome.xMonth,
+      },
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        splitLine: {
+          show: false,
+        },
+      },
+    ],
+    series: [
+      {
+        name: '园区年度收入',
+        type: 'bar',
+        barWidth: '10px',
+        data: parkIncome.yIncome.map((item, index) => {
+          const color =
+            index % 2 === 0
+              ? new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                { offset: 0.23, color: '#74c0f8' },
+                { offset: 1, color: 'rgba(116,192,248,0.00)' },
+              ])
+              : new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                { offset: 0.23, color: '#ff7152' },
+                { offset: 1, color: 'rgba(255,113,82,0.00)' },
+              ])
+          return { value: item, itemStyle: { color } }
+        }),
+      },
+    ],
+    textStyle: {
+      color: '#B4C0CC',
+    },
   }
-  getParkInfo()
+  // 3. 渲染图表
+  const myBarChart = echarts.init(barChart.value)
+  myBarChart.setOption(barOptions)
+}
 
-  // 园区收入分析
-  // 渲染年度收入分析图表
-  const barChart = ref(null) // 关联具体的容器盒子标签
-  const initBarChart = () => { // 创建图表函数
-    // 1. 解构图表数据
-    const { parkIncome } = parkInfo.value
-    // 2. == 根据业务：准备好的options数据 ==
-    const barOptions = {
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'shadow',
-        },
-      },
-      grid: {
-        // 让图表占满容器
-        top: '10px',
-        left: '0px',
-        right: '0px',
-        bottom: '0px',
-        containLabel: true,
-      },
-      xAxis: [
-        {
-          type: 'category',
-          axisTick: {
-            alignWithLabel: true,
-            show: false,
-          },
-          // x轴绑定数据
-          data: parkIncome.xMonth,
-        },
-      ],
-      yAxis: [
-        {
-          type: 'value',
-          splitLine: {
-            show: false,
-          },
-        },
-      ],
-      series: [
-        {
-          name: '园区年度收入',
-          type: 'bar',
-          barWidth: '10px',
-          data: parkIncome.yIncome.map((item, index) => {
-            const color =
-              index % 2 === 0
-                ? new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  { offset: 0.23, color: '#74c0f8' },
-                  { offset: 1, color: 'rgba(116,192,248,0.00)' },
-                ])
-                : new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  { offset: 0.23, color: '#ff7152' },
-                  { offset: 1, color: 'rgba(255,113,82,0.00)' },
-                ])
-            return { value: item, itemStyle: { color } }
-          }),
-        },
-      ],
+// 园区产业分布
+// 渲染园区产业分布图表
+const pieChart = ref(null) // 关联具体绘制到的标签
+const initPieChart = () => {
+  const { parkIndustry } = parkInfo.value
+  // == 根据业务：准备好的options数据 ==
+  const pieOption = {
+    color: [
+      '#00B2FF', '#2CF2FF', '#892CFF', '#FF624D', '#FFCF54', '#86ECA2'],
+    legend: {
+      itemGap: 20,
+      bottom: '0',
+      icon: 'rect',
+      itemHeight: 10, // 图例icon高度
+      itemWidth: 10, // 图例icon宽度
       textStyle: {
-        color: '#B4C0CC',
+        color: '#c6d1db',
       },
-    }
-    // 3. 渲染图表
-    const myBarChart = echarts.init(barChart.value)
-    myBarChart.setOption(barOptions)
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    series: [
+      {
+        name: '园区产业分析',
+        type: 'pie',
+        radius: ['55%', '60%'], // 设置内圈与外圈的半径使其呈现为环形
+        center: ['50%', '40%'], // 圆心位置， 用于调整整个图的位置
+        tooltip: {
+          trigger: 'item',
+          formatter: (params) => {
+            return `${params.seriesName}</br><div style='display:flex;justify-content: space-between;'><div>${params.marker}${params.name}</div><div>${params.percent}%</div></div>`
+          }
+        },
+        label: {
+          show: false,
+          position: 'center',
+        },
+        data: parkIndustry,
+      },
+    ],
+
   }
+
+  // 渲染
+  const myPieChart = echarts.init(pieChart.value)
+  myPieChart.setOption(pieOption)
+}
 </script>
 
 <style lang='scss' scoped>
@@ -239,6 +297,18 @@
       }
     }
   }
+
+  .section-three {
+    flex-basis: 40%;
+    margin-top: 50px;
+    .pie-chart {
+      position: relative;
+      margin: 0 auto;
+      padding-bottom: 20px;
+      width: 80%;
+      height: calc(100% - 40px);
+    }
+  }
 }
 
 .section-two {
@@ -249,6 +319,18 @@
   .bar-chart {
     width: 100%;
     height: calc(100% - 90px);
+  }
+}
+
+.section-three {
+  flex-basis: 40%;
+  margin-top: 50px;
+  .pie-chart {
+    position: relative;
+    margin: 0 auto;
+    padding-bottom: 20px;
+    width: 80%;
+    height: calc(100% - 40px);
   }
 }
 </style>
