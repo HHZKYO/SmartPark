e30712b1a6f8ed28da651c1b20d0c77009475ed3
HHZKYO <1692189552@qq.com>
2025-03-10 09:27:09 +0800

    大屏可视化-3D-2D容器添加后轨道控制器失效问题说明及解决-方案一

--- hmzs-screen/src/three3d/index.js
+++ hmzs-screen/src/three3d/index.js
@@ -18,11 +18,18 @@ export function Create3d(wrapSel) {
   this.dom.appendChild(this.renderer.domElement)
 
   // 轨道控制器
-  this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-  this.controls.enableDamping = true
-  this.controls.dampingFactor = 0.125
-  // 影响轨道控制器带着摄像机默认观察指定的坐标点位置
-  this.controls.target = new THREE.Vector3(...Object.values(CameraParams.initControlsTarget))
+  // 参数2：你让轨道控制器监听哪个标签上的鼠标事件（所以你鼠标触摸后能让轨道控制器影响摄像机的效果）
+  // 问题：轨道控制器鼠标移动不好用（是因为你监听了 canvas 画布的鼠标动作）
+  // 疑惑：为什么点击就好用，因为点击时监听 canvas-3d 的 div 标签的鼠标事件
+  // 原因：css2d 渲染器 div 盒子盖在 canvas 上面，所以你鼠标摸不到后面的 canvas 标签触发不了轨道控制器移动代码
+  // 疑惑：点击 div 盒子上鼠标事件下向父级冒泡可以触发 canvas-3d 的 div 标签父级
+
+  // this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+  // this.controls.enableDamping = true
+  // this.controls.dampingFactor = 0.125
+  // // 影响轨道控制器带着摄像机默认观察指定的坐标点位置
+  // this.controls.target = new THREE.Vector3(...Object.values(CameraParams.initControlsTarget))
+  
 
   // 坐标辅助
   this.axesHelper = new THREE.AxesHelper(500)
@@ -37,6 +44,14 @@ export function Create3d(wrapSel) {
   this.css2dRenderer.domElement.style.zIndex = 10
   // this.css2dRenderer.domElement.style.pointerEvents = 'none'
   this.dom.appendChild(this.css2dRenderer.domElement)
+
+  
+  // 方案一：
+  this.controls = new OrbitControls(this.camera, this.css2dRenderer.domElement)
+  this.controls.enableDamping = true
+  this.controls.dampingFactor = 0.125
+  // 影响轨道控制器带着摄像机默认观察指定的坐标点位置
+  this.controls.target = new THREE.Vector3(...Object.values(CameraParams.initControlsTarget))
 
 
   // 适配 3D 场景
